58ed8094ae6d493813faa798adefdddd90839bbf
prm612 <597782047@qq.com>
2019-09-11 10:24:21 +0800

    注册App

--- app.js
+++ app.js
@@ -3,18 +3,21 @@ App({
   /**
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
    */
-  onLaunch: function() {
+  onLaunch: function(options) {
+    // console.log(options);
     console.log('小程序初始化完成了：onLaunch');
+
+    // 获取用户的信息，斌给获取到用户信息之后，将用户的信息传递给服务器
     // 异步调用
     wx.getUserInfo({
       success: (res) => {
         console.log(res)
       }
     })
-    setTimeout(() => {
-      let err = new Error();
-      throw err
-    }, 3000)
+    // setTimeout(() => {
+    //   let err = new Error();
+    //   throw err
+    // }, 3000)
   },
 
   /**
@@ -22,6 +25,14 @@ App({
    */
   onShow: function(options) {
     console.log('页面显示出来了：onShow');
+    // 判断是通过什么方式打开的小程序
+    console.log(options);
+    switch(options.scene){
+      case 1001:
+        break;
+      case 1005: 
+        break;
+    };
   },
 
   /**
@@ -36,5 +47,9 @@ App({
    */
   onError: function(msg) {
     console.log('小程序中发生了错误的时候执行：onError');
+  },
+  globalData: {
+    name: 'prm612',
+    age: 18
   }
 })
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,27 +1,52 @@
+// getApp()可以获取App()产生的示例对象
+const app = getApp();
+const name = app.globalData.name;
+const age = app.globalData.age;
+console.log(name,age);
+
 Page({
-    data: {
-        name: "Prm612",
-        age: 22,
-        students: [
-            { id: 110, name: 'kobe', age: 22 },
-            { id: 120, name: 'james', age: 26 },
-            { id: 130, name: 'why', age: 18 },
-            { id: 140, name: 'honey', age: 30 },
-        ],
-        counter: 0
-    },
-    handleBtnClick() {
-        // console.log("按钮被点击");
-        let that = this;
-        that.setData({
-            counter: that.data.counter + 1
-        })
-    },
-    handleSubtraction() {
-        // console.log("按钮被点击");
-        let that = this;
-        that.setData({
-            counter: that.data.counter - 1
-        })
-    }
+  data: {
+    name: "Prm612",
+    age: 22,
+    students: [{
+        id: 110,
+        name: 'kobe',
+        age: 22
+      },
+      {
+        id: 120,
+        name: 'james',
+        age: 26
+      },
+      {
+        id: 130,
+        name: 'why',
+        age: 18
+      },
+      {
+        id: 140,
+        name: 'honey',
+        age: 30
+      },
+    ],
+    counter: 0
+  },
+  handleBtnClick() {
+    // console.log("按钮被点击");
+    let that = this;
+    that.setData({
+      counter: that.data.counter + 1
+    })
+  },
+  handleSubtraction() {
+    // console.log("按钮被点击");
+    let that = this;
+    that.setData({
+      counter: that.data.counter - 1
+    })
+  },
+  // 获取用户信息
+  handGetUserInfo(event) {
+    console.log(event)
+  }
 })
