a00a61d3ea9d9d69964f62fb4da8cede8f0dffcf
prm612 <597782047@qq.com>
2019-09-12 18:02:29 +0800

    条件判断和列表渲染

--- pages/wxml/wxml.js
+++ pages/wxml/wxml.js
@@ -10,7 +10,17 @@ Page({
     lastname: 'bryant',
     age: 20,
     nowTime: new Date().toLocaleString(),
-    isActive: false
+    isActive: false,
+    isShow: true,
+    score: 45,
+    movies: ['星际穿越','盗梦空间','大话西游'],
+    nums: [
+      [10, 14, 43, 18],
+      [5, 32, 11, 18],
+      [2, 14, 31, 50],
+      [4, 14, 98, 18]
+    ],
+    letters: ['a', 'b', 'c']
   },
   onLoad(){
     setInterval(()=>{
@@ -22,6 +32,16 @@ Page({
   handleSwitchcolor(){
     this.setData({
       isActive: !this.data.isActive
+    })
+  },
+  handelSwitchShwo(){
+    this.setData({
+      isShow: !this.data.isShow
+    })
+  },
+  handelIncrement(){
+    this.setData({
+      score: this.data.score + 6
     })
   }
 })
