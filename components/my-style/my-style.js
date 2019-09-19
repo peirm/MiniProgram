968dd28e6f34135d959ca9d95122857927da6ffa
prm612 <597782047@qq.com>
2019-09-19 10:43:47 +0800

    组件化开发

---
+++ components/my-style/my-style.js
@@ -0,0 +1,8 @@
+// components/my-style/my-style.js
+Component({
+  options: {
+    // styleIsolation: "isolated",
+    // styleIsolation: "apply-shared",
+    // styleIsolation: "shared"
+  }
+})
---
+++ pages/zdyzj/zdyzj.js
@@ -0,0 +1,19 @@
+// pages/zdyzj/zdyzj.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    counter: 0
+  },
+  handleIncrement(event){
+    console.log(event)
+    this.setData({
+      counter: this.data.counter + 1
+    })
+  },
+  handleTabClick(event){
+    console.log(event)
+  }
+})
