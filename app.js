11372cbc37bb149966449def76c8e5a5608b02e1
prm612 <597782047@qq.com>
2019-09-26 13:35:27 +0800

    登录流程

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
+const TOKEN = 'toekn';
+
 App({
+  globalData: {
+    token: ''
+  },
   // 生命周期函数：后台存活2个小时
   /**
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
@@ -18,6 +23,69 @@ App({
     //   let err = new Error();
     //   throw err
     // }, 3000)
+
+
+    // 1.先从缓存中去除token
+    const token = wx.getStorageSync(TOKEN);
+    
+    // 2.判断token是否有值
+    if(token && token.length !== 0){// 已经有token，验证token是否过期
+      this.check_token(token);
+    } else { // 没有token，进行登陆操作
+      this.login()
+    }
+  },
+  check_token(token) {
+    console.log('执行了验证token的操作')
+    wx.request({
+      url: 'http://123.207.32.32:3000/auth',
+      method: 'post',
+      header: {
+        token
+      },
+      success: (res) => {
+        if(!res.data.errCode){
+          console.log('token有效')
+          this.globalData.token = token;
+        } else {
+          cosnole.log('token无效，重新登录')
+          this.login()
+        }
+      },
+      fail: (err) => {
+        console.log(err)
+      }
+    })
+  },
+  login() {
+    console.log('执行了登陆操作')
+    // 登陆操作
+    wx.login({
+      // code只有五分钟的有效期
+      success: (res) => {
+        // 1.获取code
+        const code = res.code;
+
+        // 2.将code发送给我们的服务器
+        wx.request({
+          url: 'http://123.207.32.32:3000/login',
+          method: 'post',
+          data: {
+            code
+          },
+          success: (res) => {
+            // 1.取出token
+            const token = res.data.token
+
+            // 2.将token保存在globalData
+            this.globalData.token = token;
+
+            // 3.进行本地存储
+            wx.setStorageSync(TOKEN, token)
+          }
+        })
+      }
+    })
   },
 
   /**
